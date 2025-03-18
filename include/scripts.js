750cb3dbeca4d5179a964c9572ad42e82fb2f717
George Cave <george@interactionmagic.com>
2025-03-18 14:29:06 +0000

    Update centre location

--- include/scripts.js
+++ include/scripts.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 			}
 		}),
 		mapbox_token: 'pk.eyJ1IjoiZ2NzYWx6YnVyZyIsImEiOiJjam1pNm5uZmcwMXNyM3FtNGp6dTY3MGxsIn0.PmLPkI3T8UxjEIPnz7fxEA',
-		intersect_area: './data/tda.geojson',
+		intersect_area: './data/london-10km-square.geojson',
 		dom: {
 			mapbox: document.querySelector('.map'),
 			flightData: document.querySelector('.flight-data'),
